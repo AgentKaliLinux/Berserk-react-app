f280fe120a01b34d6e8fdf5c2f2d33302b848116
Developer <106108668+AgentKaliLinux@users.noreply.github.com>
2023-06-02 20:36:51 +0500

    Add files via upload

---
+++ src/components/Pagenotfound.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Pagenotfound = () => {
+  return (
+    <>
+    <div className='Pagenotfound'>
+      <h1>404 Error Page Not Found</h1>
+      <p>Sorry, there is not any page like this in this website.</p>
+    </div>
+    </>
+  )
+}
+
+export default Pagenotfound
